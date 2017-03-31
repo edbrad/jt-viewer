a9e7ea2859a8ede6830007e507d157054f94e54a
Edward Bradley <edbrad@edbrad.com>
2017-03-31 13:54:56 -0500

    Dashboard update - get today's jobs

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ToasterModule, ToasterService, ToasterConfig, Toast } from 'angular2-toaster';
+import { LoadingPage } from '../loading-indicator';
+
+import { DataService } from '../data.service';
+import { Observable } from "rxjs/Observable";
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent extends LoadingPage implements OnInit {
 
   private toasterService: ToasterService;
   public config1 : ToasterConfig = new ToasterConfig({
@@ -30,10 +34,14 @@ export class DashboardComponent implements OnInit {
     maintainAspectRatio: false
   };
 
+  stats: any = {};
+  todaysJobs: any[] = [];
+
   /**
    * @constructor
    */
-  constructor(toasterService: ToasterService) {
+  constructor(toasterService: ToasterService, private ds: DataService) {
+    super(true); // sets loading to true
     this.toasterService = toasterService;
    }
 
@@ -42,6 +50,18 @@ export class DashboardComponent implements OnInit {
    * @description Component initialization
    */
   ngOnInit() {
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth() + 1; //January is 0!
+    var yyyy = today.getFullYear();
+    var dateString: string = String(mm) + '/' + String(dd) + '/' + String(yyyy);
+    /** get a stats from the external REST API */
+    this.ds.getStats(dateString).subscribe((data => {
+      this.stats = data;
+      this.todaysJobs = this.stats.todaysJobs;
+      this.ready(); // sets loading to false
+    }));
+
     var toast: Toast = {
       type: 'success',
       title: 'EMS Job Ticket Viewer',
@@ -49,6 +69,16 @@ export class DashboardComponent implements OnInit {
     };
 
     this.toasterService.pop(toast);
+  }
+
+  /**
+   * @function addCommas
+   * @description reformatts a number by adding colums for thousands deliniation
+   * @param {number} intNum the number to add commas to
+   * @returns {string} the formatted number with commas (US Style)
+   */
+  private addCommas(intNum: number) {
+    return (intNum + '').replace(/(\d)(?=(\d{3})+$)/g, '$1,');
   }
 
 }
